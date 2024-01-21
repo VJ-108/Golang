3601a079404833ae2844488fc153ff3b56f2e057
VJ-108 <vaibhav160205@gmail.com>
2024-01-21 17:29:02 +0530

    Build for OS

--- 05-Time/main.go
+++ 05-Time/main.go
@@ -14,4 +14,9 @@ func main() {
 	createDate := time.Date(2024, time.January, 10, 23, 23, 0, 0, time.UTC)
 	fmt.Println(createDate)
 	fmt.Println(createDate.Format("01-02-2006 15:04:05 Monday"))
+
+
+	//Can build exe files for different os using
+	//windows - GOOS="windows" go build
+	//linux - GOOS="linux" go build
 }
